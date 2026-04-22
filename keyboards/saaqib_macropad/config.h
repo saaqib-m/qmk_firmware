5ab77e1badd2e413847becb2ac64f1e53951a8b9
Saaqib <75570134+saaqib-m@users.noreply.github.com>
2026-04-22 22:44:59 +0100

    Update config.h

--- keyboards/saaqib_macropad/config.h
+++ keyboards/saaqib_macropad/config.h
@@ -1,1 +1,22 @@
 
+#pragma once
+
+/* Encoder */
+#define ENCODER_RESOLUTION 4
+
+/* ST7789 TFT Display */
+#define ST7789_SPI_INSTANCE         spi0
+#define ST7789_DC_PIN               GP21
+#define ST7789_RST_PIN              GP20
+#define ST7789_CS_PIN               GP17
+#define ST7789_SCK_PIN              GP18
+#define ST7789_MOSI_PIN             GP19
+#define ST7789_DISPLAY_WIDTH        320
+#define ST7789_DISPLAY_HEIGHT       240
+
+/* LVGL */
+#define QUANTUM_PAINTER_LVGL_UI_WIDTH   320
+#define QUANTUM_PAINTER_LVGL_UI_HEIGHT  240
+
+/* VIA */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
